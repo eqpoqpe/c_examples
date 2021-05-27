0d4da716bff4745018f4499ac827d1180e1ad13c
Ryan Martin <54093748+Eqpoqpe@users.noreply.github.com>
2021-05-27 23:09:26 +0800

    Update max_profit.c

--- max_profit.c
+++ max_profit.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
+static unsigned int hurt_count(unsigned int *, unsigned int);
+
+int main()
+{
+        int nums[5] = {2, 5, 2, 7, 2, 9};
+        unsigned int result = hurt_count(nums, 5);
+        return 0;
+}
+
 static unsigned int hurt_count(unsigned int *_nums, unsigned int _size)
 {
         unsigned int count = 0;
