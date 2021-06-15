75cc45c516fef42f37e4057bb487f82c9a55902d
Ryan Martin <54093748+Eqpoqpe@users.noreply.github.com>
2021-06-15 22:55:31 +0800

    Create remember_free_it.c

---
+++ remember_free_it.c
@@ -0,0 +1,19 @@
+/* GNU/Linux
+ * Written by Ryan Martin <?>
+ */
+#include <stdlib.h>
+
+void *lmalloc(int);
+
+int main()
+{
+    int *ptr;
+    ptr = lmalloc(4);
+    free(ptr); /* free */
+    return 0;
+}
+
+void *lmalloc(int siz)
+{
+    return (void *) malloc(siz);
+}
